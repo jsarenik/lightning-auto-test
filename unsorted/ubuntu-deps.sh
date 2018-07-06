b80499db7c1f08f59662c5677464d3c7342f97ec
Jan Sarenik <jasan@jasan.tk>
2018-07-06 15:24:47 +0200

    Add to ubuntu-deps (from lightning README)

--- unsorted/ubuntu-deps.sh
+++ unsorted/ubuntu-deps.sh
@@ -1,5 +1,6 @@
 #apt install software-properties-common
 #add-apt-repository ppa:bitcoin/bitcoin
 apt update
+apt install -y autoconf automake build-essential git libtool libgmp-dev libsqlite3-dev python python3 net-tools zlib1g-dev
 apt install -y bitcoind
 apt install -y asciidoc valgrind python3-pip
