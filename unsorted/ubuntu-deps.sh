30a2bb8af31323aa8ebf3585e0d095cf01c9e460
Jan Sarenik <jasan@jasan.tk>
2018-07-12 17:44:58 +0200

    Update unsorted/ubuntu-deps.sh

--- unsorted/ubuntu-deps.sh
+++ unsorted/ubuntu-deps.sh
@@ -1,6 +1,15 @@
-#apt install software-properties-common
-#add-apt-repository ppa:bitcoin/bitcoin
-apt update
-apt install -y autoconf automake build-essential git libtool libgmp-dev libsqlite3-dev python python3 net-tools zlib1g-dev
-apt install -y bitcoind
-apt install -y asciidoc valgrind python3-pip
+APT=apt-get
+
+. /etc/lsb-release
+printf 'deb mirror://mirrors.ubuntu.com/mirrors.txt %s main multiverse universe restricted\n' $DISTRIB_CODENAME{,-security} > /etc/apt/sources.list
+
+$APT update
+$APT upgrade -y
+$APT install -y tmux time git
+$APT install -y autoconf automake build-essential libtool libgmp-dev libsqlite3-dev python3-dev python3-pip zlib1g-dev
+$APT install -y software-properties-common
+echo | add-apt-repository ppa:bitcoin/bitcoin
+$APT update
+$APT install -y bitcoind
+$APT install -y cppcheck shellcheck
+#apt install -y asciidoc valgrind
