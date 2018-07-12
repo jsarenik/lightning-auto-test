75bd5c461bf14f3fef1198a7147ab5c6037fc271
Jan Sarenik <jasan@jasan.tk>
2018-07-12 17:46:01 +0200

    ubuntu-deps.sh: Add busybox and link to /bin/vi

--- unsorted/ubuntu-deps.sh
+++ unsorted/ubuntu-deps.sh
@@ -5,6 +5,8 @@ printf 'deb mirror://mirrors.ubuntu.com/mirrors.txt %s main multiverse universe 
 
 $APT update
 $APT upgrade -y
+$APT install -y busybox
+ln -s busybox /bin/vi
 $APT install -y tmux time git
 $APT install -y autoconf automake build-essential libtool libgmp-dev libsqlite3-dev python3-dev python3-pip zlib1g-dev
 $APT install -y software-properties-common
