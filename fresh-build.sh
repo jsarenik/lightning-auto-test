db8649e90fcac39f3d14c7aa00d289169851662e
Jan Sarenik <jajomojo@gmail.com>
2021-02-23 16:07:24 +0100

    Add fresh-build.sh
    This fixes many issues, makes the build directory (including
    the tags) fresh, like if the repo was just cloned anew.

---
+++ fresh-build.sh
@@ -0,0 +1,9 @@
+cd ~/src/lightning
+git checkout master
+git submodule deinit --all --force
+git clean -xffd
+git tag -l | xargs git tag -d && git fetch -t
+git pull
+./configure
+make -j4
+make PREFIX=$HOME/lightning-my install
