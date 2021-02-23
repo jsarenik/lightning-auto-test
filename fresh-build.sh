56ad360b33d5ed6c36f23bf924075f3c3f704194
Jan Sarenik <jajomojo@gmail.com>
2021-02-23 17:15:18 +0100

    fresh-build.sh: simplify
    https://git-scm.com/book/en/v2/Git-Tools-Submodules

--- fresh-build.sh
+++ fresh-build.sh
@@ -6,9 +6,7 @@ git submodule deinit --all --force
 git clean -xffd
 git tag -l | xargs git tag -d && git fetch -t
 git pull
-git submodule init
-git submodule update
-git submodule sync --recursive
+git submodule update --init --recursive
 ./configure
 make -j4
 make PREFIX=$HOME/lightning-my install
